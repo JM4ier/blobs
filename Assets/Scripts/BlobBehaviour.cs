bb913eb0ae2eb91efc31b2e09351516b4f0d4181
EnigmaticCrow <1.jmaier.3@gmail.com>
2019-03-19 17:18:04 +0100

    initial commit

---
+++ Assets/Scripts/BlobBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobBehaviour : MonoBehaviour
+{
+    public Rigidbody2D rig;
+
+    [System.Serializable]
+    public struct BlobSprites
+    {
+        public SpriteRenderer body, eyes, mouthClosed, mouthOpened;
+    }
+
+    public BlobSprites sprites;
+    public Vector2 maxDisplacement = Vector2.one;
+    private Vector2 displacement;
+    public float displacementSpeed;
+
+    void Start()
+    {
+        displacement = Vector2.zero;
+        maxDisplacement.x = Mathf.Abs(maxDisplacement.x);
+        maxDisplacement.y = Mathf.Abs(maxDisplacement.y);
+    }
+
+    void Update()
+    {
+        var threshold = 0.75f;
+        var v = rig.velocity;
+        v = rig.transform.InverseTransformVector(v);
+
+        var d = displacement * Time.deltaTime;
+
+        if (v.y < -threshold)
+        {
+            sprites.eyes.flipY = false;
+            displacement.y -= d.y;
+        }
+        else if (v.y > threshold)
+        {
+            sprites.eyes.flipY = true;
+            displacement.y += d.y;
+        }
+        if (v.x < -threshold)
+        {
+            sprites.mouthOpened.flipX = true;
+            sprites.eyes.flipX = true;
+            displacement.x -= d.x;
+        }
+        else if (v.x > threshold)
+        {
+            sprites.mouthOpened.flipX = false;
+            sprites.eyes.flipX = false;
+            displacement.x += d.x;
+        }
+
+        displacement.x = Mathf.Clamp(displacement.x, -maxDisplacement.x, maxDisplacement.x);
+        displacement.y = Mathf.Clamp(displacement.y, -maxDisplacement.y, maxDisplacement.y);
+
+        sprites.eyes.transform.localPosition = displacement;
+    }
+}
---
+++ Assets/Scripts/BlobMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobMovement : MonoBehaviour
+{
+    [System.Serializable]
+    public struct MovementKeys
+    {
+        public KeyCode jump, left, right;
+    }
+
+    public Rigidbody2D blob;
+    public Transform blobBottom;
+    public MovementKeys movement;
+    public Vector2 speed = Vector2.one;
+    private bool grounded = false;
+
+    public LayerMask jumpable;
+
+    void Update()
+    {
+        grounded = false;
+        if (Physics2D.Raycast(blobBottom.position, -blobBottom.up, 0.1f, jumpable))
+        {
+            grounded = true;
+        }
+
+        var force = Vector2.zero;
+        if (Input.GetKey(movement.jump) && grounded)
+            force.y += speed.y;
+        if (Input.GetKey(movement.right))
+            force.x += speed.x;
+        if (Input.GetKey(movement.left))
+            force.x -= speed.x;
+
+        force = blob.transform.TransformVector(force);
+        blob.AddForce(force);
+    }
+}
